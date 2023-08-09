0c0e625ae108279c35902e95029f65205e641175
Jerome Offroy <jerome.offroy@gmail.com>
2023-08-09 19:22:31 +0200

    ok

--- init.bat
+++ init.bat
@@ -2,6 +2,8 @@
 setlocal enableDelayedExpansion
 
 set VERSION=2.11
+set BASE=
+set END= 
 set APPLICATION=Calcul La Grande Semaine - STROKEPLAY - Tn - HOMME_OU_DAME_v%VERSION%.xlsm
 
 set APPLICATION_PATH=..\..\LGS_Application
@@ -13,14 +15,13 @@ set APPLICATION_README_FULL=%APPLICATION_PATH%\%APPLICATION_README%
 set HELPER_PATH=fichier exemple nom export FFG
 set HELPER_FULL=%APPLICATION_PATH%\%HELPER_PATH%
 
-set tours=T1 T2 T3 T4 T5 T6
+set tours=T1 T2 T3 T4 T5 T6 Finale
+set tours_plus=%tours% T7
+set tech=Backup Poub
 
 echo Creation des repertoires
-echo "%tours%"
 
-set list=%tours% T7 Finale Backup Poub 
-echo "%lists%"
-
+set list=%tours_plus% %tech%
 (for %%x in (%list%) do ( 
   mkdir %%x 2> nul
 ))
@@ -30,8 +31,7 @@ for /f %%i in ('dir /B ..\..') do set CURRENT_YEAR=%%i
 echo Ceation de l'application pour l'annee: %CURRENT_YEAR%
 
 echo Installation de l'APPLICATION: %APPLICATION_FULL% --^>
-set list=%tours% Finale 
-(for %%a in (%list%) do ( 
+(for %%a in (%tours%) do ( 
   copy /-Y "%APPLICATION_FULL%" "Calcul La Grande Semaine - STROKEPLAY - %%a - HOMME_OU_DAME_v%VERSION%.xlsm"
 ))
 
