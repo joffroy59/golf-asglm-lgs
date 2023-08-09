215d340deec8147e3c565698a1e43ca7574ecb55
Jerome Offroy <jerome.offroy@gmail.com>
2023-08-09 02:01:25 +0200

    v1.11

--- init.bat
+++ init.bat
@@ -1,38 +1,33 @@
 @echo OFF
 setlocal enableDelayedExpansion
 
-set APPLICATION=Calcul La Grande Semaine - STROKEPLAY - Tn - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_1=Calcul La Grande Semaine - STROKEPLAY - T1 - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_2=Calcul La Grande Semaine - STROKEPLAY - T2 - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_3=Calcul La Grande Semaine - STROKEPLAY - T3 - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_4=Calcul La Grande Semaine - STROKEPLAY - T4 - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_5=Calcul La Grande Semaine - STROKEPLAY - T5 - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_6=Calcul La Grande Semaine - STROKEPLAY - T6 - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_FINAL=Calcul La Grande Semaine - STROKEPLAY - Finale - HOMME_OU_DAME_v2.10.xlsm
-set APPLICATION_README=readme.txt
+set VERSION=2.11
+set APPLICATION=Calcul La Grande Semaine - STROKEPLAY - Tn - HOMME_OU_DAME_v%VERSION%.xlsm
+
 set APPLICATION_PATH=..\..\LGS_Application
 set APPLICATION_FULL=%APPLICATION_PATH%\%APPLICATION%
+
+set APPLICATION_README=readme.txt
 set APPLICATION_README_FULL=%APPLICATION_PATH%\%APPLICATION_README%
+
 set HELPER_PATH=fichier exemple nom export FFG
 set HELPER_FULL=%APPLICATION_PATH%\%HELPER_PATH%
 
-echo Cr‚ation des r‚pertoires
-for /l %%x in (1, 1, 7) do mkdir T%%x 2> nul
-mkdir Backup 2> nul
-mkdir Poub 2> nul
+echo Crï¿½ation des rï¿½pertoires
+set list=T1 T2 T3 T4 T5 T6 T7 Finale Backup Poub 
+(for %%x in (%list%) do ( 
+  mkdir %%x 2> nul
+))
 
 rem get current year
 for /f %%i in ('dir /B ..\..') do set CURRENT_YEAR=%%i
-echo C‚ation de l'application pour l'ann‚e: %CURRENT_YEAR%
+echo Cï¿½ation de l'application pour l'annï¿½e: %CURRENT_YEAR%
 
 echo Installation de l'APPLICATION: %APPLICATION_FULL% --^>
-copy /-Y "%APPLICATION_FULL%" "%APPLICATION_1%"
-copy /-Y "%APPLICATION_FULL%" "%APPLICATION_2%"
-copy /-Y "%APPLICATION_FULL%" "%APPLICATION_3%"
-copy /-Y "%APPLICATION_FULL%" "%APPLICATION_4%"
-copy /-Y "%APPLICATION_FULL%" "%APPLICATION_5%"
-copy /-Y "%APPLICATION_FULL%" "%APPLICATION_6%"
-copy /-Y "%APPLICATION_FULL%" "%APPLICATION_FINAL%"
+set list=T1 T2 T3 T4 T5 T6 Finale 
+(for %%a in (%list%) do ( 
+  copy /-Y "%APPLICATION_FULL%" "Calcul La Grande Semaine - STROKEPLAY - %%a - HOMME_OU_DAME_v%VERSION%.xlsm"
+))
 
 copy /-Y "%APPLICATION_README_FULL%" .
 
